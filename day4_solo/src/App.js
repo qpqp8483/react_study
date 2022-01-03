de31e0ac0e86febe741b2561305dbd67945b5072
장찬우 <>
2022-01-04 00:01:01 +0900

    리액트 4일차

---
+++ day4_solo/src/App.js
@@ -0,0 +1,11 @@
+import IterationSample from './component/IterationSample';
+
+function App() {
+  return (
+    <>
+      <IterationSample></IterationSample>
+    </>
+  );
+}
+
+export default App;
