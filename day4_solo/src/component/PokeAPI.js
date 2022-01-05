8d7a246cfc80e9718abe692dae427e30f5abc29c
qpqp8483 <qpqp8483@naver.com>
2022-01-05 18:02:01 +0900

    pokeAPI 수정

--- day4_solo/src/component/PokeAPI.js
+++ day4_solo/src/component/PokeAPI.js
@@ -9,16 +9,19 @@ const PokeAPI = () => {
         let url;
 
         function fetchPoke(){
-            url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=200';
+            url = 'https://pokeapi.co/api/v2/type/3';
             return fetch(url).then(function(response){
                 return response.json();
+            }).catch(() => {
+                console.log('주소를 찾을수 없습니다.');
             });
         };
 
         async function getPoke() {
             const response = await fetchPoke();
-            console.log(response.results[0].name);  
-            setPokeDatas(response.results);
+            console.log(response); 
+            console.log(response.pokemon[0].pokemon.name);  
+            setPokeDatas(response.pokemon);
             console.log(pokeDatas);
         };
 
@@ -26,9 +29,10 @@ const PokeAPI = () => {
 
     }, [])
 
-    const pokeName = pokeDatas.map(pokeData => 
-        <li key={pokeData.name}>
-            {pokeData.name}
+    const pokeName = pokeDatas.map((pokeData, index) => //id값이 존재하지 않으므로 index로 대체, but key값으로는 index사용 지양
+        <li key={index}> 
+            <span>포켓몬 이름 : {pokeData.pokemon.name}</span> <br />
+            <span>포켓몬 순서 : {index}</span>
         </li>
     )
 
