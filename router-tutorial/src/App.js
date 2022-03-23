d9dcf7092b3a5e64eec6422b13d6b7bf7736ff00
chanwoo <wkdcksdn1996@gmail.com>
2022-03-23 19:33:33 +0900

    스터디 9회차

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Profiles from './components/Profiles';
+import HistorySample from './components/HistorySample';
+
 
 
 const App = () => {
@@ -10,10 +13,14 @@ const App = () => {
       <ul>
         <li><Link to="/">홈</Link></li>
         <li><Link to="/about">소개</Link></li>
+        <li><Link to="/profiles">프로필</Link></li>
+        <li><Link to="/history">history 예제</Link></li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
+      <Route path="/profiles" component={Profiles}/>
+      <Route path="/history" component={HistorySample}/>
     </div>
   );
 }
